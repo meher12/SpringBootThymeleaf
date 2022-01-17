0fb4f1ed06e5a86f777e7fa258b828ee8398b87b
meher12 <meher.khemiri@gmail.com>
2022-01-17 16:08:49 +0100

    Thymeleaf SpringBoot Project

---
+++ src/main/java/guru/springthymeleaf/SpringthymeleafApplication.java
@@ -0,0 +1,13 @@
+package guru.springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringthymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springthymeleaf/SpringthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringthymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
